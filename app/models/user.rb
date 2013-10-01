60b4e685b8a4743117e589bb7e1581f6767c4276
Pat O'Neill <pdoneill26@gmail.com>
2013-09-30 21:41:37 -0400

    Styled flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,27 @@
 class User < ActiveRecord::Base
+has_many :statuses
+
+ 	validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates :profile_name, presence: true,
+                           uniqueness: true,
+                           format: {
+                            with: /a-zA-Z0-9_-/,
+                            message: 'Must be formatted correctly'
+                           }
+
+
+
+  def full_name
+  first_name + " " + last_name 
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 
 end
